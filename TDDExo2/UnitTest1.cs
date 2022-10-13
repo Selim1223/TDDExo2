a8a9494cf0a5bd52b02dd990304d7241f4a5946d
Selim1223 <91537143+Selim1223@users.noreply.github.com>
2022-10-13 09:20:38 +0200

    Update UnitTest1.cs

--- TDDExo2/UnitTest1.cs
+++ TDDExo2/UnitTest1.cs
@@ -1,46 +1,49 @@
 namespace TDDExo2
 {
     [TestClass]
-    public class UnitTest1
+     public class UnitTest1
     {
+        private Calculator _calculator;
+
+        public UnitTest1()
+        {
+            _calculator = new Calculator();
+        }
+
+
         [TestMethod]
         [DataRow(10, 20, 30)]
         public void AddTwoElements_ShouldReturnRightResult(int firstNumber, int secondNumber, int result)
         {
-            var calculator = new Calculator();
-            Assert.AreEqual(calculator.Add(firstNumber, secondNumber), result);
+            Assert.AreEqual(_calculator.Add(firstNumber, secondNumber), result);
         }
 
         [TestMethod]
         [DataRow(30, 20, 10)]
         public void SubTwoElements_ShouldReturnRightResult(int firstNumber, int secondNumber, int result)
         {
-            var calculator = new Calculator();
-            Assert.AreEqual(calculator.Sub(firstNumber, secondNumber), result);
+            Assert.AreEqual(_calculator.Sub(firstNumber, secondNumber), result);
         }
 
         [TestMethod]
         [DataRow(5, 2, 10)]
         public void MultiplyTwoElements_ShouldReturnRightResult(int firstNumber, int secondNumber, int result)
         {
-            var calculator = new Calculator();
-            Assert.AreEqual(calculator.Multiply(firstNumber, secondNumber), result);
+            Assert.AreEqual(_calculator.Multiply(firstNumber, secondNumber), result);
         }
 
         [TestMethod]
         [DataRow(15, 3, 5)]
         public void DivideTwoElements_ShouldReturnRightResult(int firstNumber, int secondNumber, int result)
         {
-            var calculator = new Calculator();
-            Assert.AreEqual(calculator.Divide(firstNumber, secondNumber), result);
+            Assert.AreEqual(_calculator.Divide(firstNumber, secondNumber), result);
         }
 
         [TestMethod]
-        [DataRow(15, 0, 5)]
-        public void DivideTwoElementsByZero_ShouldReturnRightResult(int firstNumber, int secondNumber, int result)
+        [DataRow(15, 0)]
+        public void DivideTwoElementsByZero_ShouldThrowException(int firstNumber, int secondNumber)
         {
-            var calculator = new Calculator();
-            Assert.ThrowsException<DivideByZeroException>(() => calculator.Divide(firstNumber,secondNumber));
+            Assert.ThrowsException<CustomDivideByZeroException>(() => _calculator.Divide(firstNumber,secondNumber));
         }
     }
 }
